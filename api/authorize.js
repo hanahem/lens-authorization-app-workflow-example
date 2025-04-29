97eaf6daee9129daaa078609fade92f0ff3edd05
Wassim <wawa.h8@gmail.com>
2025-04-29 13:48:20 +0100

    fix: all origins

--- api/authorize.js
+++ api/authorize.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // or '*' for public
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Type, Origin');
 
